2572bf8b71e5c95422f762e6047996b1fb23fbcb
Favionetah <faviosandy30@gmail.com>
2026-06-26 14:31:03 -0400

    fix: redirect to dist/ con hash routing y guard contra loop

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/Potato-Ship/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/Potato-Ship/dist/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
