124f8c375bb973c9ffb4ad5cfd4e554edb576272
Favionetah <faviosandy30@gmail.com>
2026-06-26 13:55:31 -0400

    fix: base path usa GITHUB_ACTIONS para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.CI ? '/Potato-Ship/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/Potato-Ship/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
