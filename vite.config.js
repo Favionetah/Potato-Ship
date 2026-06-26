95b0bd6c04828eff2347dd67aaa88f8216ebc32f
Favionetah <faviosandy30@gmail.com>
2026-06-26 13:20:13 -0400

    fix: base path condicional - CI vs local

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/Potato-Ship/',
+  base: process.env.CI ? '/Potato-Ship/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
